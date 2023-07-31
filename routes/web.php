409f34aebafe0de388757176f6004132849974c6
npv34 <phanvanluan611996@gmail.com>
2023-07-31 14:24:47 +0700

    init project

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    echo "<h2>This is Home page</h2>";
+});
+
+Route::get('/about', function () {
+    echo "<h2>This is About page</h2>";
+});
+
+Route::get('/contact', function () {
+    echo "<h2>This is Contact page</h2>";
+});
+
+Route::get('/users/{name}', function ($name) {
+    echo "<h2>User name is $name</h2>";
+});
